37b306bed9ec531626b1994819b21fffcc34c3b6
Brian Gao <bhgao20014@gmail.com>
2021-08-26 16:22:10 -0400

    Added short description to README; started basic outline of tic tac toe ruby file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# tic-tac-toe
+# Tic-Tac-Toe
+
+A terminal-based version of tic-tac-toe. It is played with two players from the command line, allowing you to choose your symbol (going to implement later) and replay the game as many times as you want. Hope you like it!
---
+++ main.rb
@@ -0,0 +1,53 @@
+class TicTacToe
+  include Validation
+
+  attr_accessor :board
+  attr_reader :game_over
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @board = [
+      [nil, nil, nil],
+      [nil, nil, nil],
+      [nil, nil, nil],
+    ]
+    @game_over = false
+  end
+
+  def play_turn(player)
+    move = player.choose_move(board)
+    until valid_move?(move)
+      move = player.choose_move(board)
+    end
+    update_board(move)
+  end
+
+  def update_board
+end
+
+class Player 
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
+
+  def choose_move(board)
+  end
+end
+
+def tic_tac_toe()
+  loop do
+    player1 = Player.new("George", "X")
+    player2 = Player.new("Bob", "O")
+    tic_tac_toe = TicTacToe.new(player1, player2)
+    until tic_tac_toe.game_over
+      tic_tac_toe.play_turn(player1)
+      break if tic_tac_toe.game_over?
+      tic_tac_toe.play_turn(player2)
+      break if tic_tac_toe.game_over?
+  end
+end
+
+  
+
